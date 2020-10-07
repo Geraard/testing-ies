1d89219cbd594087db3d2143db407c04a1034ee4
gerard <gerardesbri40@gmail.com>
2020-10-07 09:58:17 +0200

    New line febrer

---
+++ index.php
@@ -0,0 +1,4 @@
+echo 'Al gener, tanca la porta i encén el braser';
+echo 'Aigua de gener tot l'any va bé';
+echo 'Les gelades de gener fan florir l'ametller';
+echo 'Les gelades de gener fan florir l'ametller';
